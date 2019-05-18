60bcccdd91ba77781a30cd76c9caff2e9d1585e2
Moody Salem <moody.salem@gmail.com>
2019-05-18 14:34:00 -0400

    add another test

--- contracts/Tracker.sol
+++ contracts/Tracker.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.21 <0.6.0;
 
 /// The tracker stores all the metadata hashes for the tracker.
 contract Tracker {
-  event FileMetadataAdded(address indexed creator, bytes32 ipfsHash);
+  event FileMetadataAdded(address indexed creator, uint256 index, uint256 timestamp, bytes32 ipfsHash);
 
   struct FileMetadata {
     bytes32 ipfsHash;
@@ -11,19 +11,21 @@ contract Tracker {
   }
 
   FileMetadata[] public allFileMetadata;
-  int public numFileMetadata;
+  uint256 public numFileMetadata;
 
   /// Add a file with the given IPFS metadata hash.
   /// Hash is expected to be a sha256 multihash with the 2 byte prefix removed, i.e. a simple sha256 hash
   function addFile(bytes32 ipfsHash) public {
+    uint256 timestamp = block.timestamp;
+
     allFileMetadata.push(FileMetadata({
       ipfsHash : ipfsHash,
       creator : msg.sender,
-      timestamp : block.timestamp
+      timestamp : timestamp
       }));
 
-    numFileMetadata ++;
+    emit FileMetadataAdded(msg.sender, numFileMetadata, timestamp, ipfsHash);
 
-    emit FileMetadataAdded(msg.sender, ipfsHash);
+    numFileMetadata ++;
   }
 }
--- test/tracker.test.ts
+++ test/tracker.test.ts
@@ -16,6 +16,25 @@ contract('TrackerContract', ([ deployer, ac1, ac2 ]) => {
       expect(tx.logs).to.be.an('array').with.length(1);
       expect(tx.logs[ 0 ].args.ipfsHash).to.eq('0x0abc000000000000000000000000000000000000000000000000000000000000');
       expect(tx.logs[ 0 ].args.creator).to.eq(ac1);
+      expect(tx.logs[ 0 ].args.index.toNumber()).to.eq(0);
+      expect(tx.logs[ 0 ].args.timestamp.toNumber()).to.not.eq(0);
+    });
+
+    it('adds it to the list', async () => {
+      const {logs} = await tracker.addFile('0xabc', {from: ac1});
+
+      const struct = await tracker.allFileMetadata(logs[ 0 ].args.index);
+
+      expect(struct[ 0 ]).to.eq('0x0abc000000000000000000000000000000000000000000000000000000000000');
+      expect(struct[ 1 ]).to.eq(ac1);
+    });
+
+  });
+
+  describe('allFileMetadata', async () => {
+    let index;
+    beforeEach(async () => {
+      index = (await tracker.addFile('0xabc', {from: ac1})).logs[ 0 ].args.index;
     });
   });
 
